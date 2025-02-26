c246d47eec7217749386084eab1b6fe082a6b58b
Kimi Osti <kimi.osti@studio.unibo.it>
2025-02-26 15:34:35 +0100

    Added dummy project main class

---
+++ control-unit-backend/src/main/java/unibo/esiot2024/Application.java
@@ -0,0 +1,17 @@
+package unibo.esiot2024;
+
+/**
+ * Launcher class for the backend application.
+ */
+public final class Application {
+
+    private Application() { }
+
+    /**
+     * Launcher method for the backend application.
+     * @param args eventual command line arguments passed to the application.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
