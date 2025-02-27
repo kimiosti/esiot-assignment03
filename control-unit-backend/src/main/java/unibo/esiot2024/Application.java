2077398fcd54a5c6f983ac9803ace2b390cd1c7d
Kimi Osti <kimi.osti@studio.unibo.it>
2025-02-27 19:05:16 +0100

    Modified main class to support GUI database authentication

--- control-unit-backend/src/main/java/unibo/esiot2024/Application.java
+++ control-unit-backend/src/main/java/unibo/esiot2024/Application.java
@@ -1,5 +1,9 @@
 package unibo.esiot2024;
 
+import java.sql.SQLException;
+
+import unibo.esiot2024.central.db_access.impl.DatabaseAccessHandlerImpl;
+
 /**
  * Launcher class for the backend application.
  */
@@ -8,10 +12,34 @@ public final class Application {
     private Application() { }
 
     /**
-     * Launcher method for the backend application.
+     * Main method for the backend application.
      * @param args eventual command line arguments passed to the application.
      */
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
+        new GUI();
+    }
+
+
+    /**
+     * Launches the backend application.
+     * @param dbUser the username for the database connection.
+     * @param dbPass the password for the database connetcion.
+     * @throws SQLException
+    */
+    public static void launch(final String dbUser, final String dbPass) {
+        //boolean excepted = false;
+        try {
+            new DatabaseAccessHandlerImpl(dbUser, dbPass);
+        } catch (final SQLException e) {
+            //excepted = true;
+            new GUI();
+        }
+
+        /**
+         * TODO
+         * qui inizializza le altre componenti del controller assicurandoti che l'inizializzazione del database vada
+         * a buon fine. Sopra solo il controller centrale con l'accesso diretto al DB.
+         */
+        /*if (!excepted) { }*/
     }
 }
