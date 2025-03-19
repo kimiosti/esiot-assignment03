a9577f3db6caa911e87a1c074d09b2e6481338d2
Kimi Osti <kimi.osti@studio.unibo.it>
2025-03-19 18:45:28 +0100

    Modified import directive and fixed warning related to signed to unsigned type comparison

--- window-controller/src/timer_impl.cpp
+++ window-controller/src/timer_impl.cpp
@@ -1,11 +1,11 @@
-#include "timer_impl.h"
+#include "scheduler/timer_impl.h"
 
 TimerImpl::TimerImpl() {
     this->timer = new Timer();
 }
 
 void TimerImpl::waitForNextTick(long period) {
-    while (this->timer->read() < period) { }
+    while (this->timer->read() < (unsigned) period) { }
     this->timer->pause();
     this->timer->start();
 }
