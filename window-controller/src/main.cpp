b82709968f19130f8c74adc5ffae09675df1ed9d
Kimi Osti <kimi.osti@studio.unibo.it>
2025-03-20 20:06:56 +0100

    Modified main to initialize operator output task

--- window-controller/src/main.cpp
+++ window-controller/src/main.cpp
@@ -2,6 +2,9 @@
 #include "scheduler/scheduler.h"
 #include "scheduler/coop_r_r_scheduler.h"
 #include "scheduler/task.h"
+//#include "window_controller/window_controlling_task.h"
+#include "operator_output/operator_output_task.h"
+//#include "operator_input/operator_input_task.h"
 #include "communication/communication_task.h"
 #include "dirty_state_tracker.h"
 #include "state_tracker.h"
@@ -9,7 +12,7 @@
 Scheduler *scheduler;
 Task *communication_task;
 //Task *window_controlling_task;
-//Task *operator_output_task;
+Task *operator_output_task;
 //Task *operator_input_task;
 DirtyStateTracker *dirty_state_tracker;
 StateTracker *state_tracker;
@@ -21,12 +24,12 @@ void setup() {
   scheduler = new CoopRRScheduler(50);
 
   //window_controlling_task = new WindowControllingTask(200, dirty_state_tracker, state_tracker);
-  //operator_output_task = new OperatorOutputTask(50, dirty_state_tracker, state_tracker);
+  operator_output_task = new OperatorOutputTask(100, state_tracker);
   //operator_input_task = new OperatorInputTask(50, dirty_state_tracker, state_tracker);
   communication_task = new CommunicationTask(200, dirty_state_tracker, state_tracker);
 
   //scheduler->bind(window_controlling_task);
-  //scheduler->bind(operator_output_task);
+  scheduler->bind(operator_output_task);
   //scheduler->bind(operator_input_task);
   scheduler->bind(communication_task);
 }
