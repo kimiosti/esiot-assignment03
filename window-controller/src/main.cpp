ec17fd00b929f184aa9c821ec5244cf239b39884
Kimi Osti <kimi.osti@studio.unibo.it>
2025-03-25 22:52:47 +0100

    Added instantiation of operator input task

--- window-controller/src/main.cpp
+++ window-controller/src/main.cpp
@@ -4,7 +4,7 @@
 #include "scheduler/task.h"
 //#include "window_controller/window_controlling_task.h"
 #include "operator_output/operator_output_task.h"
-//#include "operator_input/operator_input_task.h"
+#include "operator_input/operator_input_task.h"
 #include "communication/communication_task.h"
 #include "dirty_state_tracker.h"
 #include "state_tracker.h"
@@ -13,7 +13,7 @@ Scheduler *scheduler;
 Task *communication_task;
 //Task *window_controlling_task;
 Task *operator_output_task;
-//Task *operator_input_task;
+Task *operator_input_task;
 DirtyStateTracker *dirty_state_tracker;
 StateTracker *state_tracker;
 
@@ -25,12 +25,12 @@ void setup() {
 
   //window_controlling_task = new WindowControllingTask(200, dirty_state_tracker, state_tracker);
   operator_output_task = new OperatorOutputTask(100, state_tracker);
-  //operator_input_task = new OperatorInputTask(50, dirty_state_tracker, state_tracker);
+  operator_input_task = new OperatorInputTask(50, dirty_state_tracker);
   communication_task = new CommunicationTask(200, dirty_state_tracker, state_tracker);
 
   //scheduler->bind(window_controlling_task);
   scheduler->bind(operator_output_task);
-  //scheduler->bind(operator_input_task);
+  scheduler->bind(operator_input_task);
   scheduler->bind(communication_task);
 }
 
