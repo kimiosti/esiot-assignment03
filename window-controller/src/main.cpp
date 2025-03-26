f2d9c4dd3dd6d97f87bda254c876b1540e0a46ce
Kimi Osti <kimi.osti@studio.unibo.it>
2025-03-26 22:27:02 +0100

    Modified main to istantiate window controlling task

--- window-controller/src/main.cpp
+++ window-controller/src/main.cpp
@@ -2,7 +2,7 @@
 #include "scheduler/scheduler.h"
 #include "scheduler/coop_r_r_scheduler.h"
 #include "scheduler/task.h"
-//#include "window_controller/window_controlling_task.h"
+#include "window_controller/window_controlling_task.h"
 #include "operator_output/operator_output_task.h"
 #include "operator_input/operator_input_task.h"
 #include "communication/communication_task.h"
@@ -11,7 +11,7 @@
 
 Scheduler *scheduler;
 Task *communication_task;
-//Task *window_controlling_task;
+Task *window_controlling_task;
 Task *operator_output_task;
 Task *operator_input_task;
 DirtyStateTracker *dirty_state_tracker;
@@ -23,12 +23,12 @@ void setup() {
   state_tracker = new StateTracker();
   scheduler = new CoopRRScheduler(50);
 
-  //window_controlling_task = new WindowControllingTask(200, dirty_state_tracker, state_tracker);
+  window_controlling_task = new WindowControllingTask(200, state_tracker);
   operator_output_task = new OperatorOutputTask(100, state_tracker);
   operator_input_task = new OperatorInputTask(50, dirty_state_tracker);
   communication_task = new CommunicationTask(200, dirty_state_tracker, state_tracker);
 
-  //scheduler->bind(window_controlling_task);
+  scheduler->bind(window_controlling_task);
   scheduler->bind(operator_output_task);
   scheduler->bind(operator_input_task);
   scheduler->bind(communication_task);
