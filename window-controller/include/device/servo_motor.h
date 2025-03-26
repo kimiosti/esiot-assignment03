fd387c375e285a5aa7a42462aede8e39dcaeb017
Kimi Osti <kimi.osti@studio.unibo.it>
2025-03-26 22:09:13 +0100

    Added window implementation via servo motor

---
+++ window-controller/include/device/servo_motor.h
@@ -0,0 +1,19 @@
+#ifndef __SERVO_MOTOR__
+#define __SERVO_MOTOR__
+
+#include <ServoTimer2.h>
+#include "window.h"
+
+class ServoMotor: public Door {
+    public:
+        ServoMotor(int pin);
+        void lock();
+        void unlock();
+        void openToLevel(int percentage);
+
+    private:
+        ServoTimer2 *motor;
+        int pin;
+};
+
+#endif
---
+++ window-controller/src/servo_motor.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <ServoTimer2.h>
+#include "device/servo_motor.h"
+
+#define MIN_PULSE 750
+#define PERCENTILE_SIZE 150
+
+ServoMotor::ServoMotor(int pin) {
+    this->pin = pin;
+    this->motor = new ServoTimer2();
+}
+
+void ServoMotor::lock() {
+    this->motor->detach();
+}
+
+void ServoMotor::unlock() {
+    if (!this->motor->attached()) {
+        this->motor->attach(this->pin);
+    }
+}
+
+void ServoMotor::openToLevel(int percentage) {
+    this->motor->write(MIN_PULSE + (percentage * PERCENTILE_SIZE));
+}
