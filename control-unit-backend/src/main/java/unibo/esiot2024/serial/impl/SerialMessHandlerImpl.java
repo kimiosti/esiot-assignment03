0fb133ac6c17d6112a54f3a3cc24c193a366d3b3
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-08 17:20:49 +0200

    Modified outgoing message structure and added non-null check to incoming reads

--- control-unit-backend/src/main/java/unibo/esiot2024/serial/impl/SerialMessHandlerImpl.java
+++ control-unit-backend/src/main/java/unibo/esiot2024/serial/impl/SerialMessHandlerImpl.java
@@ -1,5 +1,6 @@
 package unibo.esiot2024.serial.impl;
 
+import java.util.Locale;
 import java.util.Optional;
 
 import unibo.esiot2024.serial.api.SerialMessHandler;
@@ -15,20 +16,22 @@ public final class SerialMessHandlerImpl implements SerialMessHandler {
     private static final int MODE_SWITCH_END = 1;
     private static final int OPENING_PERCENTAGE_BEGIN = 2;
     private static final int OPENING_PERCENTAGE_END = 5;
+    private static final String TEMPERATURE_FORMAT = "%02.2f";
+    private static final String OPENING_FORMAT = "%03d";
 
     @Override
     public String assembleMess(final float temperature, final SystemState curState, final int openingPercentage) {
         return "T"
-            + temperature
+            + String.format(TEMPERATURE_FORMAT, temperature).replace(',', '.')
             + " M"
-            + (curState.equals(SystemState.MANUAL) ? "0" : "1")
+            + (curState.equals(SystemState.MANUAL) ? "1" : "0")
             + " O"
-            + openingPercentage;
+            + String.format(OPENING_FORMAT, openingPercentage, Locale.US);
     }
 
     @Override
     public Optional<SerialRead> parseMess(final String message) {
-        if (message.length() == INCOMING_MESS_LENGTH) {
+        if (message != null && message.length() == INCOMING_MESS_LENGTH) {
             return Optional.of(new SerialRead(
                 "t".equals(message.substring(MODE_SWITCH_BEGIN, MODE_SWITCH_END)),
                 Integer.parseInt(message.substring(OPENING_PERCENTAGE_BEGIN, OPENING_PERCENTAGE_END))
