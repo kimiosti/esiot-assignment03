f68418fe3cf8efeed72b10cbb5684c9e823df53d
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-09 15:27:40 +0200

    Fixed step count reset and removed tolerance check in opening percentage variation

--- window-controller/src/window_controlling_task.cpp
+++ window-controller/src/window_controlling_task.cpp
@@ -1,24 +1,24 @@
 #include "window_controller/window_controlling_task.h"
 #include "device/servo_motor.h"
 
-#define WINDOW_PIN 11
-#define TOLERANCE 5
-#define LOCK_TIME 300
+#define WINDOW_PIN 3
+#define LOCK_TIME 1000
 
 WindowControllingTask::WindowControllingTask(long period, StateTracker *state_tracker) {
     this->period = period;
     this->step_count = 0;
     this->idle_step_count = 0;
     this->state_tracker = state_tracker;
-    this->last_measure = this->state_tracker->getOpeningPercentage();
+    this->last_measure = -1;
     this->window = new ServoMotor(WINDOW_PIN);
 }
 
 void WindowControllingTask::step(long sched_period) {
     this->step_count++;
     if (this->step_count * sched_period >= this->period) {
+        this->step_count = 0;
         int percentage = this->state_tracker->getOpeningPercentage();
-        if (abs(percentage - this->last_measure) >= TOLERANCE) {
+        if (percentage != this->last_measure) {
             this->idle_step_count = 0;
             this->last_measure = percentage;
             this->window->unlock();
