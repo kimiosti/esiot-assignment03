03dfadbe79466bbfb59093cc55ea777e2b99cbc3
Kimi Osti <kimi.osti@studio.unibo.it>
2025-04-11 18:15:56 +0200

    Added empty implementation for MQTT message handler

---
+++ control-unit-backend/src/main/java/unibo/esiot2024/mqtt/impl/MQTTMessHandlerImpl.java
@@ -0,0 +1,22 @@
+package unibo.esiot2024.mqtt.impl;
+
+import unibo.esiot2024.mqtt.api.MQTTMessHandler;
+
+/**
+ * Implementation for the {@link MQTTMessHandler} interface.
+ */
+public final class MQTTMessHandlerImpl implements MQTTMessHandler {
+
+    @Override
+    public MQTTRead parseMess(String mess) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parseMess'");
+    }
+
+    @Override
+    public String assembleMess(float frequency) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assembleMess'");
+    }
+    
+}
