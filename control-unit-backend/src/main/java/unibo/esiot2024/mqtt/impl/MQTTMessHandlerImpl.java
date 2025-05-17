d596b2c0d4bc926cd046498775162ac65048175a
Kimi Osti <kimi.osti@studio.unibo.it>
2025-05-17 17:03:57 +0200

    Implemented MQTT agent message parsing and assembling logic

--- control-unit-backend/src/main/java/unibo/esiot2024/mqtt/impl/MQTTMessHandlerImpl.java
+++ control-unit-backend/src/main/java/unibo/esiot2024/mqtt/impl/MQTTMessHandlerImpl.java
@@ -1,5 +1,10 @@
 package unibo.esiot2024.mqtt.impl;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
 import unibo.esiot2024.mqtt.api.MQTTMessHandler;
 import unibo.esiot2024.utils.TemperatureMeasure;
 
@@ -8,16 +13,31 @@ import unibo.esiot2024.utils.TemperatureMeasure;
  */
 public final class MQTTMessHandlerImpl implements MQTTMessHandler {
 
+    private static final int TEMP_INDEX = 1;
+    private static final int DATE_INDEX = 3;
+    private static final int TIME_INDEX = 5;
+    private static final String SPLIT_REGEX = "[{}, ]";
+    private static final String PREFIX = "{ ";
+    private static final String POSTFIX = " }";
+    private static final String FREQUENCY_LABEL = "\"frequency\": ";
+
     @Override
-    public TemperatureMeasure parseMess(String mess) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parseMess'");
+    public TemperatureMeasure parseMess(final String mess) {
+        final var words = new ArrayList<String>(List.of(mess.split(SPLIT_REGEX)));
+        words.removeIf(String::isEmpty);
+        return new TemperatureMeasure(
+            Float.parseFloat(words.get(TEMP_INDEX)),
+            Date.valueOf(words.get(DATE_INDEX)),
+            Time.valueOf(words.get(TIME_INDEX))
+        );
     }
 
     @Override
-    public String assembleMess(int frequency) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'assembleMess'");
+    public String assembleMess(final int frequency) {
+        return PREFIX
+            + FREQUENCY_LABEL
+            + frequency
+            + POSTFIX;
     }
-    
+
 }
