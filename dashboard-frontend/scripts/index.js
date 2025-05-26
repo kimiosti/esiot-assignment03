dba59aa7cbc65e37eb6dd5b61fb9e9b0f85c1c0e
Kimi Osti <kimi.osti@studio.unibo.it>
2025-05-26 15:23:12 +0200

    Modified JavaScript for index page to properly interact with backend

--- dashboard-frontend/scripts/index.js
+++ dashboard-frontend/scripts/index.js
@@ -1,4 +1,17 @@
+function toggleMode() {
+    fetch("api/action/toggleMode", {
+        method: "POST"
+    });
+}
+
+function restoreState() {
+    fetch("api/action/restoreState", {
+        method: "POST"
+    });
+}
+
 function refresh() {
+    // TODO - revise refresh operations
     fetch("api/data", {
         method: "POST"
     }).then(res => {
@@ -11,37 +24,24 @@ function refresh() {
                 labels: data.times,
                 datasets: [{
                     label: 'Temperature',
-                    data: data.temp,
+                    data: data.temps,
                 }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
             }
         });
+
+        document.getElementById("avgMeasure").innerHTML = data.average;
+        document.getElementById("maxMeasure").innerHTML = data.max;
+        document.getElementById("minMeasure").innerHTML = data.min;
+        document.getElementById("curState").innerHTML = data.state;
+        document.getElementById("openingLevel").innerHTML = data.opening;
     });
 }
 
 const chartArea = document.getElementById("chart");
-var chart = new Chart(chartArea, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Temperature',
-            data: [],
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+var chart = new Chart(chartArea, { });
+refresh();
+
+document.getElementById("modeSwitch").addEventListener("click", toggleMode);
+document.getElementById("resetState").addEventListener("click", restoreState);
 
 setInterval(refresh, 2000);
